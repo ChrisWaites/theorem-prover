ce5f0a2a1e052ae99b5ef381836f2034392ca634
cwaites3 <cwaites3@gatech.edu>
2016-08-18 23:44:13 -0400

    Reformat comments.

--- src/NumberTheory.py
+++ src/NumberTheory.py
@@ -1,18 +1,16 @@
 from Node import Node
 
-# Ax(Ay(p(x, y))) => Ay(Ax(p(x, y)))
-# Ex(Ey(p(x, y))) => Ey(Ex(p(x, y)))
-# Ax(Ey(p(x, y))) => Ey(Ax(p(x, y)))
-# Ex(Ay(p(x, y))) => Ay(Ex(p(x, y)))
+# Ax(Ay(p(x, y))) <=> Ay(Ax(p(x, y)))
+# Ex(Ey(p(x, y))) <=> Ey(Ex(p(x, y)))
+# Ax(Ey(p(x, y))) <=> Ey(Ax(p(x, y)))
+# Ex(Ay(p(x, y))) <=> Ay(Ex(p(x, y)))
 def quantifierInterchange(node):
     if node.data[0] == "A" or node.data[0] == "E":
         if node.children[0].data[0] == "A" or node.children[0].data[0] == "E":
             return Node(node.children[0].data , Node(node.data, node.children[0].children[0]))
 
-# ~(Ax(p(x))) => Ex(~(p(x)))
-# ~(Ex(p(x))) => Ax(~(p(x)))
-# Ax(~(p(x))) => ~(Ex(p(x)))
-# Ex(~(p(x))) => ~(Ax(p(x)))
+# Ex(~(p(x))) <=> ~(Ax(p(x)))
+# Ax(~(p(x))) <=> ~(Ex(p(x)))
 def quantifierNegation(node):
     if node.data == "~":
         if node.children[0].data[0] == "A":
@@ -24,17 +22,17 @@ def quantifierNegation(node):
     if node.data[0] == "E" and node.children[0].data == "~":
         return Node("~", Node("E" + node.data[1:], node.children[0].children[0]))
 
-# (x)=(y) => (y)=(x)
+# (x)=(y) <=> (y)=(x)
 def symmetryOfEquality(node):
     if node.data == "=":
         return Node("=", node.children[1], node.children[0])
 
-# (x)=(y) => (S(x))=(S(y))
+# (x)=(y) <=> (S(x))=(S(y))
 def addition(node):
     if node.data == "=":
         return Node("=", Node("S", node.children[0]), Node("S", node.children[1]))
 
-# (S(x))=(S(y)) => (x)=(y)
+# (S(x))=(S(y)) <=> (x)=(y)
 def subtraction(node):
     if node.data == "=":
         if node.children[0].data == "S" and node.children[1].data == "S":
