2b028e8ee3ac16d93449af6dc4abb3f9892eb325
Chris Waites <cwaites3@gatech.edu>
2018-03-23 02:04:34 -0700

    Update tests.

--- tests/test_search.py
+++ tests/test_search.py
@@ -5,19 +5,20 @@ from theoremprover.search import *
 class TestSearch(unittest.TestCase):
 
     def test_astar(self):
-        neighbors = {
+        g = Graph({
             'a': ['b', 'c'],
             'b': ['a', 'c'],
             'c': ['a', 'b', 'd'],
             'd': ['c']
-        }
-        g = Graph(neighbors)
+        })
 
+        # shortest path are adjacent nodes (unit weights)
         self.assertEquals(
             a_star(g, ['c'], lambda s: s == 'd'),
             ['c', 'd']
         )
 
+        # only assert length is correct
         path = a_star(g, ['a'], lambda s: s == 'd')
         self.assertTrue(
             len(path) == 3 and path[0] == 'a' and path[2] == 'd'
--- tests/test_theorem_search.py
+++ tests/test_theorem_search.py
@@ -1,9 +1,12 @@
 import unittest
 from theoremprover.theorem_search import *
+from theoremprover.expression import parse
 
 
 class TestTheoremSearch(unittest.TestCase):
 
     def test_find_theorem(self):
-        self.assertEqual('', '')
+        # simply passes by reaching a solution which terminates
+        self.assertTrue(len(find_theorem(parse("~(Ea((0)=((a)+(1))))"))) >= 2)
+
 
